0baab066727de75178a81756b38f87cc85b37095
unknown <judefiadzawoo@gmail.com>
2026-05-14 14:18:20 +0000

    db query correction

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -3,9 +3,12 @@ from channels.db import database_sync_to_async
 from urllib.parse import parse_qs
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from .models import Message, Room
+from django.shortcuts import get_object_or_404
+
 
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
+    active_users={}
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
@@ -26,45 +29,86 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
             }
         )
 
+        if self.room_name not in ChatConsumer.active_users:
+            ChatConsumer.active_users[self.room_name]=set()
+        
+        ChatConsumer.active_users[self.room_name].add(self.username)
+
+        
+
     async def disconnect(self, close_code):
+        
         await self.channel_layer.group_send(
             self.room_group_name, {
                 "type": "user.leave",
                 "username": self.username
             }
         )
+        ChatConsumer.active_users[self.room_name].discard(self.username)
         await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
         raise channels.exceptions.StopConsumer()
 
     async def receive_json(self, content):
-        message = content["message"]
+        event_type=content.get("type")
 
-        await self.save_message(message)
+        if event_type == "message":
+            message = content.get("message")
+            if message:
+                 await self.save_message(message)
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    "type": "chat.message",
+                    "message": message,
+                    "username": self.username
+                }
+            )
+           
 
-        await self.channel_layer.group_send(
-            self.room_group_name, {
-                "type": "chat.message",
-                "message": message,
-                "username": self.username
-            }
-        )
+        elif event_type == "reaction":
+            emoji = content.get("emoji")
+            if emoji:
+                await self.channel_layer.group_send(
+                self.room_group_name, {
+                    "type": "chat.reaction",
+                    "emoji": emoji,
+                    "username": self.username
+                }
+            )
+
+            
+       
+       
+        
 
     @database_sync_to_async
     def save_message(self, message):
-        room = Room.objects.get(name=self.room_name)
+        room,created = Room.objects.get_or_create(name=self.room_name)
         Message.objects.create(
             room=room,
             username=self.username,
             content=message
         )
 
+    
     async def chat_message(self, event):
         await self.send_json({
+            "type":"message",
             "message": event["message"],
             "username": event["username"]
         })
+
+    
+    async def chat_reaction(self, event):
+        await self.send_json({
+            "type":"reaction",
+            "emoji": event["emoji"],
+            "username": event["username"]
+        })
+    
+    
+
 
     async def user_join(self, event):
         await self.send_json({
