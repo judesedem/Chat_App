3309633519606abe342203dba9d8569f9ce5f3a3
unknown <judefiadzawoo@gmail.com>
2026-05-14 12:39:51 +0000

    Switched to jsonwebsocketconsumer

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -1,10 +1,9 @@
-import json
+import channels.exceptions
 from urllib.parse import parse_qs
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
-
-class ChatConsumer(AsyncWebsocketConsumer):
+class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
@@ -22,14 +21,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
+        raise channels.exceptions.StopConsumer()
 
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+    async def receive_json(self, content):
+        message = content["message"]
 
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message,"username":self.username}
+            self.room_group_name, {
+                "type": "chat.message",
+                "message": message,
+                "username": self.username
+            }
         )
 
     async def chat_message(self, event):
-        await self.send(text_data=json.dumps({"message": event["message"],"username":event["username"]}))
+        await self.send_json({
+            "message": event["message"],
+            "username": event["username"]
+        })
